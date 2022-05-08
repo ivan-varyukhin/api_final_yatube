1a9c383771f38b58038efe7fa0e2b6614d1c1c52
Иван Варюхин <ivan@varyukhin.ru>
2022-05-08 14:46:49 +0300

    rollback model

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,107 +1,29 @@
-﻿from django.db import models
-from django.contrib.auth import get_user_model
-from django.db.models import UniqueConstraint
+﻿from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
-class Group(models.Model):
-    title = models.CharField(
-        max_length=200,
-        verbose_name='Группа',
-        help_text='Название группы'
-    )
-    slug = models.SlugField(
-        unique=True,
-        verbose_name='Идентификатор',
-        help_text='Идентификатор'
-    )
-    description = models.TextField(
-        verbose_name='Описание',
-        help_text='Описание группы'
-    )
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Группа'
-        verbose_name_plural = 'Группы'
-
-
 class Post(models.Model):
-    text = models.TextField(
-        verbose_name='Текст сообщения',
-        help_text='Текст сообщения'
-    )
-    pub_date = models.DateTimeField(
-        verbose_name='Дата публикации',
-        help_text='Дата публикации',
-        auto_now_add=True
-    )
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор',
-        help_text='Автор сообщения',
-        related_name='posts'
-    )
-    group = models.ForeignKey(
-        Group,
-        blank=True,
-        null=True,
-        on_delete=models.SET_NULL,
-        verbose_name='Группа',
-        help_text='Группа',
-        related_name='posts'
-    )
+        User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
-        'Картинка',
-        upload_to='posts/',
-        blank=True
-    )
-
-    class Meta:
-        ordering = ('-pub_date', )
-        verbose_name = 'Публикация'
-        verbose_name_plural = 'Публикации'
+        upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
-        return self.text[:30]
+        return self.text
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Сообщение',
-        help_text='Сообщение'
-    )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор',
-        help_text='Автор'
-    )
-    text = models.TextField(
-        verbose_name='Комментарий',
-        help_text='Текст комментария'
-    )
+        User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
     created = models.DateTimeField(
-        verbose_name='Дата комментария',
-        help_text='Дата комментария',
-        auto_now_add=True
-    )
-
-    class Meta:
-        ordering = ('-created',)
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return self.text[:30]
+        'Дата добавления', auto_now_add=True, db_index=True)
 
 
 class Follow(models.Model):
